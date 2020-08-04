eacf54ca159c5571658dcdc9054b0509f2312b7f
Corbin Hall <hallcorbin92@gmail.com>
2020-08-04 06:50:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,23 +5,24 @@ var chocolateBars = [
   'skittles'
 ]
 
-  var Array = array
-
-
 function addElementToBeginningOfArray(array, element) {
+  var Array = array
   return [element, ...Array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
+  var Array = array
   array.unshift(element)
   return Array
 }
 
 function addElementToEndOfArray(array, element) {
+  var Array = array
   return [...Array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
+  var Array = array
   array.push(element)
   return Array
 }
