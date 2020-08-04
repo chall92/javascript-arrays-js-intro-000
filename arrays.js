33065aed42afe114b0c4ab1a0eebbcb38425bd01
Corbin Hall <hallcorbin92@gmail.com>
2020-08-04 07:10:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -40,6 +40,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element) {
 
 function removeElementFromBeginningOfArray(array) {
   var Array = array
-  Array.slice(1)
+  Array.slice(0)
   return Array
 }
