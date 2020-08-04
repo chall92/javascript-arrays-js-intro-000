3455fce4cecbb0b98b5090644fc8f2592302ab1f
Corbin Hall <hallcorbin92@gmail.com>
2020-08-04 07:52:50 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -38,7 +38,8 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-
+  array.pop(-1)
+  return array
 }
 
 function removeElementFromEndOfArray(array) {
