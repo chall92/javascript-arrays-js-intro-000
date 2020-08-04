462e4c3afd19534697c68665b9db517901a335d6
Corbin Hall <hallcorbin92@gmail.com>
2020-08-04 07:25:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,6 +11,7 @@ function addElementToBeginningOfArray(array, element) {
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
+  return array
 }
 
 function addElementToEndOfArray(array, element) {
